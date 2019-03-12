bc855dd799856de6066664a0191f1df08fe59693
Lev <221349@student.pwr.edu.pl>
2019-03-12 23:43:53 +0100

    Lab3: bilinear algorhytm

--- lab3/lab3.m
+++ lab3/lab3.m
@@ -5,8 +5,8 @@ clear;
 
 IMG = imread('dummy-reversal-featured.jpg');
 %image(IMG)
-
-%image(nearest(IMG,50,50))\
+imwrite(bilinear(IMG,900,900), 'u1.png')
+imwrite(nearest(IMG,900,900), 'u2.png')
 
 
 
@@ -16,6 +16,30 @@ function out = nearest(in,Xr,Yr)
     for y = 1:Yr
         for x = 1:Xr
             out(y,x,:) = in(round(y * Yo / Yr) , round(x * Xo / Xr) , :);
+        end
+    end
+end
+
+function out = bilinear(in,Xr,Yr)
+    Xo = length(in(1,:,1)) -1;
+    Yo = length(in(:,1,1)) -1;
+    for yP = 1:Yr
+        for xP = 1:Xr
+            %out(y,x,:) = in(round(y * Yo / Yr) , round(x * Xo / Xr) , :);
+            y = yP * Yo / Yr;
+            x = xP * Xo / Xr;
+            y1 = round(y);
+            y2 = y1 + 1;
+            x1 = round(x);
+            x2 = x1 + 1;
+            
+            X = [(x2 - x), (x - x1)];
+            Y = [(y2 - y); (y - y1)];
+            Q = [in(y1, x1, :), in(y2, x1, :);  in(y1, x2, :), in(y2, x2, :)];
+            for d = 1:3
+                T = double(Q(:,:,d)) * (Y);
+                out(yP,xP,d) = uint8(X * T);
+            end
         end
     end
 end
