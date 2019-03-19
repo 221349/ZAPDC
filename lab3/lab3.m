b7971b929377970363d18576923c882d60c0171a
Lev <221349@student.pwr.edu.pl>
2019-03-19 23:08:18 +0100

    lab3: rotation:bilinear added:

--- lab3/lab3.m
+++ lab3/lab3.m
@@ -9,7 +9,7 @@ XR = 220;
 YR = 220;
 
 
-imwrite(v_nearest(IMG, [1, 2], -230), 'c1.png')
+imwrite(v_bilinear(IMG, [-1.3, 1.5], 15), 'c1.png')
 
 % 
 % for i = 1:0
@@ -29,7 +29,7 @@ imwrite(v_nearest(IMG, [1, 2], -230), 'c1.png')
 
 
 
-%% Functions:
+%% Main Functions:
 
 function out = nearest(in,Xr,Yr)
     Xo = length(in(1,:,1)) -1;
@@ -67,9 +67,7 @@ function out = bilinear(in,Xr,Yr)
     end
 end
 
-function out = rot(in, theta)
-    out = in * [cosd(theta) -sind(theta); sind(theta) cosd(theta)];
-end
+
 
 function out = v_nearest(in, vector, theta)
     Xo = length(in(1,:,1));
@@ -90,12 +88,53 @@ function out = v_nearest(in, vector, theta)
             if check_location(pos, Xo, Yo)
                 out(y,x,:) = in(pos(2), pos(1), :);
             else
-                out(y,x,:) = uint8([0, 0, 0]);
+                out(y,x,:) = uint8([255, 255, 255]); %bg-color (white)
             end
         end
     end
 end
 
+
+function out = v_bilinear(in, vector, theta)
+    Xo = length(in(1,:,1));
+    Yo = length(in(:,1,1));
+    Base = [[0 0] , 
+            rot([0, Yo*vector(2)], theta) ,
+            rot([Xo*vector(1),  Yo*vector(2)], theta) ,
+            rot([Xo*vector(1),  0], theta)];
+    offset_x = round( min(Base(:,1)) );
+    offset_y = round( min(Base(:,2)) );
+    size_x = abs( offset_x - round( max(Base(:,1)) ) );
+    size_y = abs( offset_y - round( max(Base(:,2)) ) );
+    
+    for y = 1:size_y
+        for x = 1:size_x
+            pos = r_transform([x + offset_x, y + offset_y] , vector, theta) +1;
+            
+            if check_location(pos, Xo, Yo)
+                y1 = fix(pos(2));
+                y2 = y1 + 1;
+                x1 = fix(pos(1));
+                x2 = x1 + 1;
+            
+                X = [(x2 - pos(1)), (pos(1) - x1)];
+                Y = [(y2 - pos(2)); (pos(2) - y1)];
+                Q = [in(y1, x1, :), in(y2, x1, :);  in(y1, x2, :), in(y2, x2, :)];
+                for d = 1:3
+                    out(y,x,d) = uint8((X) * double(Q(:,:,d)) * (Y));
+                end
+            else
+                out(y,x,:) = uint8([255, 255, 255]); %bg-color (white)
+            end
+        end
+    end
+end
+
+%% Helpers:
+
+function out = rot(in, theta)
+    out = in * [cosd(theta) -sind(theta); sind(theta) cosd(theta)];
+end
 
 function out = r_transform(vector_in, vector_s, theta)
     t = rot(vector_in, -theta);
