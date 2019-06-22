a35b0665b70ed18c9ab2b1b339fa7412ea897abf
LS <221349@student.pwr.edu.pl>
2019-06-22 18:14:58 +0200

    lab6: img fix

--- lab6/lab6.py
+++ lab6/lab6.py
@@ -1,16 +1,21 @@
 #!/usr/bin/env python
 
+import os, sys
 import matplotlib.image as mpimg 
 import matplotlib.pyplot as plt
 import numpy as np
+import math
 
+################################
+## mosaic/DEmosaic functions: ##
+################################
 def mosaic(img, mask):
     mask_lx = len(mask[0])
     mask_ly = len(mask)
     img_lx = len(img[0])
     img_ly = len(img)
-    out = np.array([[0]*len(img[0])]*len(img), dtype='float')
-    #out = [[0 for i in range(10)] for j in range(20)] 
+#    out = np.array([[0]*img_lx]*img_ly, dtype='float')
+    out = np.zeros((img_ly,img_lx), 'float')
     for y in range(img_ly):
         for x in range(img_lx):
             out[y][x] = (img[y][x][0]*mask[y%mask_ly][x%mask_lx][0] +
@@ -19,7 +24,55 @@ def mosaic(img, mask):
     
     return out
 
+def demosaic(img, mask):
+    mask_lx = len(mask[0])
+    mask_ly = len(mask)
+    img_lx = len(img[0]) - 1
+    img_ly = len(img) - 1
+    out = np.zeros((img_ly,img_lx,3), 'float')
+    #np.array([[3]*img_lx]*img_ly, dtype='float')
+    #print (img[50][60])
+    z = 0
+    for y in range(img_ly):
+        for x in range(img_lx):
+            for c in range(3):
+                out[y][x][c] = (img[y][x]*mask[y%mask_ly][x%mask_lx][c] + 
+                                img[y][x+1]*mask[y%mask_ly][(x+1)%mask_lx][c] +
+                                img[y+1][x]*mask[(y+1)%mask_ly][x%mask_lx][c] + 
+                                img[y+1][x+1]*mask[(y+1)%mask_ly][(x+1)%mask_lx][c]) / (
+                                    mask[y%mask_ly][x%mask_lx][c] + 
+                                    mask[y%mask_ly][(x+1)%mask_lx][c] +
+                                    mask[(y+1)%mask_ly][x%mask_lx][c] + 
+                                    mask[(y+1)%mask_ly][(x+1)%mask_lx][c])
+    return out
 
+def demosaic_adv(img, mask, window, step):
+    mask_lx = len(mask[0])
+    mask_ly = len(mask)
+    img_lx = math.trunc( (len(img[0]) - (window[1] - 1) ) / step[1])
+    img_ly = math.trunc( (len(img) - (window[0] - 1) ) / step[0])
+    out = np.zeros((img_ly,img_lx,3), 'float')
+    #np.array([[3]*img_lx]*img_ly, dtype='float')
+    #print (img[50][60])
+    z = 0
+    for ly in range(img_ly):
+        y = ly * step[0]
+        for lx in range(img_lx):
+            x = lx * step[1]
+            for c in range(3):
+                csum = 0
+                msum = 0
+                for wy in range(window[0]):
+                    for wx in range (window[1]):
+                        csum += img[y + wy][x + wx]*mask[(y+wy)%mask_ly][(x+wx)%mask_lx][c]
+                        msum += mask[(y+wy)%mask_ly][(x+wx)%mask_lx][c]
+                out[ly][lx][c] = csum/msum
+    return out
+
+
+###########
+## CFAs: ##
+###########
 def BAYER_MASK():
     return [
         [[0,1,0], [1,0,0]],
@@ -38,12 +91,22 @@ def XTRANS_MASK():
             ]
 
 
+os.makedirs( "sprawozdanie/img", exist_ok=True)
+
 img = mpimg.imread('4demosaicking.png') 
+mos = mosaic(img, BAYER_MASK())
+dmos = demosaic(mos, BAYER_MASK())
+mpimg.imsave('sprawozdanie/img/bayer_mos.png', demosaic_adv(mos, BAYER_MASK(), [1, 1], [1, 1])) 
+mpimg.imsave('sprawozdanie/img/bayer_dmos.png', dmos) 
 
-plt.imshow(mosaic(img, XTRANS_MASK()), cmap='gray',  interpolation='nearest') 
-plt.show()
+mos = mosaic(img, XTRANS_MASK())
+dmos = demosaic(mos, XTRANS_MASK())
+mpimg.imsave('sprawozdanie/img/xtrans_mos.png', demosaic_adv(mos, XTRANS_MASK(), [1, 1], [1, 1])) 
+mpimg.imsave('sprawozdanie/img/xtrans_dmos.png', dmos) 
 
-
-#print (img.shape)
-#plt.imshow(img) 
+dmos = demosaic_adv(mos, XTRANS_MASK(), [3, 3], [1, 1])
+mpimg.imsave('sprawozdanie/img/xtrans_dmos_adv_w3.png', dmos)
+ 
+#plt.imshow(dmos, cmap='gray',  interpolation='nearest') 
+#plt.imshow(dmos)
 #plt.show()
